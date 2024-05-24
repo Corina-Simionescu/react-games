00c41399e28c7ff61f6341b32540d41a91fe6256
Corina717 <corina.simionescu12@gmail.com>
2024-05-24 19:16:38 +0300

    added guess-color game and finished the project

--- proiect-practica/src/App.jsx
+++ proiect-practica/src/App.jsx
@@ -1,33 +1,73 @@
 import React from 'react';
 import './App.css';
 import TicTacToe from './TicTacToe';
+import GuessColor from './GuessColor';
 
 
 function App()
 {
+    function scrollToSection(sectionId)
+    {
+        const section = document.getElementById(sectionId);
+        if (section)
+        {
+            section.scrollIntoView({ behavior: 'smooth' });
+        }
+    }
+
     return (
-        <>
+        <section className='App'>
             <header>
-                {/* <ion-icon name="game-controller-outline"></ion-icon> */}
-                <ion-icon name="game-controller"></ion-icon>
-                <h2 className="title">Games</h2>
-                <button>Contact</button>
-                <button>About</button>
+                <section className='logo'>
+                    <ion-icon name="game-controller-outline"></ion-icon>
+                    <h2 className="title">Games</h2>
+                </section>
+
+                <section className='contact-about-buttons'>
+                    <button onClick={() => scrollToSection('contact')}>Contact</button>
+                    <button onClick={() => scrollToSection('about')}>About</button>
+                </section>
             </header>
 
             <section className="choose-game">
                 <h1>Choose a game</h1>
-                <button>TIC TAC TOE</button>
-                <button>GUESS THE COLOR</button>
-                {/* poate pun icons: flaticons */}
+                <section className='games-buttons'>
+                    <button onClick={() => scrollToSection('tic-tac-toe')}>TIC TAC TOE</button>
+                    <button onClick={() => scrollToSection('guess-color')}>GUESS THE COLOR</button>
+                </section>
             </section>
 
-            <section className="tic-tac-toe">
+            <section className='tic-tac-toe' id='tic-tac-toe'>
                 <TicTacToe />
             </section>
 
-        </>
+            <section className='guess-color' id='guess-color'>
+                <GuessColor />
+            </section>
+
+            <section className='about' id='about'>
+                <h1>About</h1>
+                <p>Welcome to Games! This website is a personal project created by me, Corina Simionescu. I made it in order to showcase my skills in web development and React programming.</p>
+                <p>Feel free to explore the games on this site. If you have any feedback, questions, or suggestions for improvement, don't hesitate to send me a message.</p>
+                <p>Thank you for visiting!</p>
+            </section>
+
+            <section className='contact' id='contact'>
+                <h1>Contact</h1>
+                <p>Feel free to reach out to me via:</p>
+                <ul>
+                    <li>GitHub: <a href="https://github.com/Corina-Simionescu">Corina-Simionescu</a></li>
+                    <li>LinkedIn: <a href="https://www.linkedin.com/in/corina-simionescu/">Corina Simionescu</a></li>
+                    <li>Email: corina.simionescu12@gmail.com</li>
+                </ul>
+            </section>
+
+            <footer className="footer">
+                <p>&copy; 2024 Corina Simionescu</p>
+            </footer>
+
+        </section>
     )
 }
 
-export default App
+export default App;
--- proiect-practica/src/TicTacToe.jsx
+++ proiect-practica/src/TicTacToe.jsx
@@ -30,31 +30,31 @@ const TicTacToe = () =>
   }
 
   const getWinner = () =>
+  {
+    const winningLines =
+      [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+      ];
+
+    for (let i = 0; i < winningLines.length; i++)
     {
-      const winningLines =
-        [
-          [0, 1, 2],
-          [3, 4, 5],
-          [6, 7, 8],
-          [0, 3, 6],
-          [1, 4, 7],
-          [2, 5, 8],
-          [0, 4, 8],
-          [2, 4, 6]
-        ];
-    
-      for (let i = 0; i < winningLines.length; i++)
+      const [square1, square2, square3] = winningLines[i];
+
+      if (squaresValues[square1] != null && squaresValues[square1] === squaresValues[square2] && squaresValues[square1] === squaresValues[square3])
       {
-        const [square1, square2, square3] = winningLines[i];
-    
-        if (squaresValues[square1] != null && squaresValues[square1] === squaresValues[square2] && squaresValues[square1] === squaresValues[square3])
-        {
-          return squaresValues[square1];
-        }
+        return squaresValues[square1];
       }
-    
-      return null;
     }
+
+    return null;
+  }
 
   const hasNullElements = array =>
   {
@@ -78,31 +78,33 @@ const TicTacToe = () =>
   }
 
   const restartGame = () =>
-    {
-      const nullArray = Array(9).fill(null);
-      setSquaresValues(nullArray);
-      setOIsNext(false);
-    }
+  {
+    const nullArray = Array(9).fill(null);
+    setSquaresValues(nullArray);
+    setOIsNext(false);
+  }
 
   return (
     <>
+      <h1 className='title'>TIC TAC TOE</h1>
       <h2 className='message'>{message}</h2>
-      <section className="row">
-        <Square squareValue={squaresValues[0]} onSquareClick={() => handleSquareClick(0)} />
-        <Square squareValue={squaresValues[1]} onSquareClick={() => handleSquareClick(1)} />
-        <Square squareValue={squaresValues[2]} onSquareClick={() => handleSquareClick(2)} />
-      </section>
-      <section className="row">
-        <Square squareValue={squaresValues[3]} onSquareClick={() => handleSquareClick(3)} />
-        <Square squareValue={squaresValues[4]} onSquareClick={() => handleSquareClick(4)} />
-        <Square squareValue={squaresValues[5]} onSquareClick={() => handleSquareClick(5)} />
-      </section>
-      <section className="row">
-        <Square squareValue={squaresValues[6]} onSquareClick={() => handleSquareClick(6)} />
-        <Square squareValue={squaresValues[7]} onSquareClick={() => handleSquareClick(7)} />
-        <Square squareValue={squaresValues[8]} onSquareClick={() => handleSquareClick(8)} />
-      </section>
-      <button onClick={restartGame}>Restart</button>
+        <section className="row">
+          <Square squareValue={squaresValues[0]} onSquareClick={() => handleSquareClick(0)} />
+          <Square squareValue={squaresValues[1]} onSquareClick={() => handleSquareClick(1)} />
+          <Square squareValue={squaresValues[2]} onSquareClick={() => handleSquareClick(2)} />
+        </section>
+        <section className="row">
+          <Square squareValue={squaresValues[3]} onSquareClick={() => handleSquareClick(3)} />
+          <Square squareValue={squaresValues[4]} onSquareClick={() => handleSquareClick(4)} />
+          <Square squareValue={squaresValues[5]} onSquareClick={() => handleSquareClick(5)} />
+        </section>
+        <section className="row">
+          <Square squareValue={squaresValues[6]} onSquareClick={() => handleSquareClick(6)} />
+          <Square squareValue={squaresValues[7]} onSquareClick={() => handleSquareClick(7)} />
+          <Square squareValue={squaresValues[8]} onSquareClick={() => handleSquareClick(8)} />
+        </section>
+
+      <button className='restart' onClick={restartGame}>Restart</button>
     </>
   )
 }
